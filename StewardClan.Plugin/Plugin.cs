d08306234a1bfabdaa3f670e11f6397544fe141a
Jackson Levitt <47047592+crazyjackel@users.noreply.github.com>
2025-03-18 21:58:22 -0500

    Cards are done

--- StewardClan.Plugin/Plugin.cs
+++ StewardClan.Plugin/Plugin.cs
@@ -11,6 +11,7 @@ namespace Steward_Clan.Plugin
     public class Plugin : BaseUnityPlugin
     {
         internal static new ManualLogSource Logger = new(MyPluginInfo.PLUGIN_GUID);
+
         public void Awake()
         {
             // Plugin startup logic
@@ -23,17 +24,28 @@ namespace Steward_Clan.Plugin
                 {
                     c.AddMergedJsonFile(
                         "plugin.json",
-                        "champion_albert.json",
-                        "character_bigger_steward.json",
-                        "card_breaking_point.json",
-                        "card_back_to_basic.json",
-                        "card_galvanize.json",
-                        "card_gravitas.json",
-                        "card_mechanical_outrage.json",
-                        "card_overclock.json",
-                        "card_pyre_refactor.json",
-                        "card_steamburst.json"
-                        );
+                        "champions/champion_albert.json",
+                        "characters/character_bigger_steward.json",
+                        "characters/character_wrench_head.json",
+                        "cards/card_back_to_basic.json",
+                        "cards/card_breaking_point.json",
+                        "cards/card_bypass.json",
+                        "cards/card_cache.json",
+                        "cards/card_decompile.json",
+                        "cards/card_disarm.json",
+                        "cards/card_galvanize.json",
+                        "cards/card_gravitas.json",
+                        "cards/card_mechanical_outrage.json",
+                        "cards/card_overclock.json",
+                        "cards/card_process_data.json",
+                        "cards/card_pyre_refactor.json",
+                        "cards/card_scrap.json",
+                        "cards/card_steamburst.json",
+                        "cards/card_steward_spike.json",
+                        "cards/card_synthesize.json",
+                        "cards/card_try_catch.json",
+                        "cards/card_writeline.json"
+                    );
                 }
             );
 
